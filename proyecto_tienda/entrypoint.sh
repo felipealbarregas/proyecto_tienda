0315cc8754842033a3730573ff3224c90354fbc2
Felipe <felipegarba2011@hotmail.es>
2024-04-28 20:38:34 +0200

    creacion base de datos

---
+++ proyecto_tienda/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Esperar a que Postgres esté disponible
+while ! nc -z db 5432; do
+  echo "Esperando a que PostgreSQL inicie..."
+  sleep 0.1
+done
+echo "PostgreSQL iniciado"
+
+# Ejecutar migraciones
+echo "Aplicando migraciones..."
+python manage.py migrate
+
+# Iniciar Gunicorn
+echo "Iniciando Gunicorn..."
+exec gunicorn --workers 3 --bind 0.0.0.0:8000 myproject.wsgi:application
